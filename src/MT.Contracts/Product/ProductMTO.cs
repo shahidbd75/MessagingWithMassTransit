795ea577f5464a92b84e4ed9bd01cbbe2afa279e
Abdul Baset <abdul.baset@iqvia.com>
2023-05-15 16:31:58 +0600

    Minimal api implement

---
+++ src/MT.Contracts/Product/ProductMTO.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MT.Contracts.Product;
+
+public record ProductCreation(string Name, string CategoryId, DateTime CreatedAt);
+
+public record ProductModification(string Name, string CategoryId, DateTime UpdateAt);
+
+public record ProductDeletion(string Name);
---
+++ src/MT.Publisher/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MT.Publisher.Data;
+using MT.Publisher.Data.Models;
+
+namespace MT.Publisher.Services
+{
+    public static class ProductService
+    {
+        public static async Task<IResult> AddProductAsync(Product product, StoreDbContext context)
+        {
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
+
+            return Results.Created($"products/{product.Name}", product);
+        }
+
+        public static async Task<IResult> GetAllProductsAsync(StoreDbContext context)
+        {
+            return Results.Ok(await context.Products.ToListAsync());
+        }
+
+        public static async Task<IResult> GetProductsAsync(string name, StoreDbContext context)
+        {
+            var product = await context.Products.FirstOrDefaultAsync(p => p.Name == name);
+            
+            
+            return product != null ? Results.Ok(product): Results.NotFound();
+        }
+
+        public static async Task<IResult> DeleteProductAsync(int id, StoreDbContext context)
+        {
+            var product = await context.Products.FindAsync(id);
+
+            if (product == null)
+                return TypedResults.NotFound();
+
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+
+            return TypedResults.NoContent();
+        }
+    }
+}
